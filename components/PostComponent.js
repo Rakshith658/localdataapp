6a3725e504e274290cfd1e74f53c4c093d40a15c
Rakshith658 <rakshithkumars7777@gmail.com>
2021-10-24 13:54:28 +0530

    compiled successfully

--- components/PostComponent.js
+++ components/PostComponent.js
@@ -38,6 +38,13 @@ export default PostComponent;
 
 const styles = StyleSheet.create({
   postContainer: {
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
     elevation: 10,
     backgroundColor: "#fff",
     marginHorizontal: 10,
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -134,6 +134,13 @@ const styles = StyleSheet.create({
   },
   createContainer: {
     margin: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
     elevation: 50,
     backgroundColor: "#fff",
     borderRadius: 10,
